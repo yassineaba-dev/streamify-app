15951abf9ea60e3ce3c3d1db0b7c1859f672cfa2
Yassine Aba <yassineaba.dev@gmail.com>
2025-08-07 15:00:04 +0100

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_URL + "/api";
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true
+});
